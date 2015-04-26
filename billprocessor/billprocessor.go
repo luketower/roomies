b81290fe544211b44deec0400e85047fd04cfef7
Luke Tower <lukegtower@gmail.com>
2015-04-26 12:57:19 -0400

    Add functionality to read file
    - Move main logic into its own package
    - Add billsreader to read file and feed arguments to
      billprocessor
    
    This commit adds ability to either add all arguments on
    command line or read them in from a file.

---
+++ billprocessor/billprocessor.go
@@ -0,0 +1,171 @@
+package billprocessor
+
+import (
+	a "github.com/mgutz/ansi"
+	"log"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+var (
+	MONTHS = map[string]string{
+		"01": "January",
+		"02": "February",
+		"03": "March",
+		"04": "April",
+		"05": "May",
+		"06": "June",
+		"07": "July",
+		"08": "August",
+		"09": "September",
+		"10": "October",
+		"11": "November",
+		"12": "December",
+	}
+)
+
+func ErrorMsg(args []string) string {
+	return "\n" +
+		a.Color(lineBreak("*", 70), "yellow") + "\n" +
+		a.Color(lineBreak("*", 70), "yellow") + "\n\n" +
+		a.Color("There was a problem with your inputs:\n", "red") + "\n" +
+		"  '" + strings.Join(args, " ") + "'\n\n" +
+		a.Color("Input should resemble the following:\n", "red") + "\n" +
+		"  'date 12/2015 Gas 34.55 Electric 45.99 Rent 933 -- bob 45 susan 55'\n\n" +
+		"* You must include the date!\n" +
+		"* You must add '--' followed by name/percentage pairs.\n" +
+		"  Ex. '(args) -- bob 45 susan 55'\n\n" +
+		a.Color(lineBreak("*", 70), "yellow") + "\n" +
+		a.Color(lineBreak("*", 70), "yellow") + "\n"
+}
+
+func BillReport(args []string) string {
+	bills := map[string]string{}
+	billsMap(args, bills)
+	return a.Color(monthHeader(args), "blue") + "\n" +
+		a.Color(lineBreak("*", 25), "green") + "\n" +
+		eachBill(bills) +
+		a.Color(lineBreak("-", 25), "green") + "\n" +
+		billToString("Total", calcTotal(bills)) +
+		a.Color(lineBreak("-", 25), "green") + "\n" +
+		individualShares(calcTotal(bills), args)
+}
+
+func lineBreak(char string, num int) string {
+	return strings.Repeat(char, num)
+}
+
+func monthHeader(args []string) string {
+	dateArr := strings.Split(args[1], "/")
+	return MONTHS[dateArr[0]] + " " + dateArr[1]
+}
+
+func billsMap(args []string, billsMap map[string]string) {
+	for i, s := range args {
+		if s == "--" {
+			break
+		}
+		if isMonth(s, args) {
+			continue
+		}
+		if i%2 != 0 {
+			key := args[i-1]
+			billsMap[key] = s
+		}
+	}
+}
+
+func isMonth(s string, args []string) bool {
+	return s == "date" ||
+		s == "month" ||
+		strings.Contains(s, "/") && s == args[1]
+}
+
+func eachBill(bills map[string]string) (allBills string) {
+	names := orderKeys(bills)
+	for _, n := range names {
+		allBills += billToString(strings.Title(n), bills[n])
+	}
+	return
+}
+
+func billToString(name, amount string) string {
+	floatAmount, err := strconv.ParseFloat(amount, 64)
+	if err != nil {
+		log.Fatal("OUCH! ", err)
+	}
+	return name + ":" + tabsFor(name) +
+		strconv.FormatFloat(floatAmount, 'f', 2, 64) + "\n"
+}
+
+func tabsFor(text string) (tabs string) {
+	tabs = "\t$"
+	if len(text) <= 5 {
+		tabs = "\t" + tabs
+	}
+	return
+}
+
+func calcTotal(bills map[string]string) string {
+	var total float64
+	for _, v := range bills {
+		val, _ := strconv.ParseFloat(v, 64)
+		total += val
+	}
+	return strconv.FormatFloat(total, 'f', 2, 64)
+}
+
+func individualShares(total string, args []string) (totalShares string) {
+	shares := map[string]string{}
+	calcShares(args, total, shares)
+	names := orderKeys(shares)
+	for _, name := range names {
+		billName := strings.Title(name) + "'s Total"
+		totalShares += billToString(billName, shares[name])
+	}
+	return
+}
+
+func calcShares(args []string, total string, shares map[string]string) {
+	stringArgs := strings.Join(args, " ")
+	stringShares := strings.Split(stringArgs, " -- ")[1]
+	sharesArr := strings.Split(stringShares, " ")
+	for i, s := range sharesArr {
+		if i%2 != 0 {
+			key := sharesArr[i-1]
+			shares[key] = calcShare(s, total)
+		}
+	}
+}
+
+func calcShare(percent string, total string) string {
+	percentFloat, err := strconv.ParseFloat(percent, 64)
+	totalFloat, err := strconv.ParseFloat(total, 64)
+	if err != nil {
+		log.Fatal("OUCH! ", err)
+	}
+	share := totalFloat * (percentFloat / 100.00)
+	return strconv.FormatFloat(share, 'f', 2, 64)
+}
+
+func Valid(args []string) bool {
+	hasDate := includeIn(args, "date") || includeIn(args, "month")
+	hasDash := includeIn(args, "--")
+	hasMinimumCount := len(args) >= 9
+	isOdd := len(args)%2 != 0
+	return hasDate && hasDash && hasMinimumCount && isOdd
+}
+
+func includeIn(args []string, flag string) bool {
+	stringArgs := strings.Join(args, " ")
+	return strings.Contains(stringArgs, flag)
+}
+
+func orderKeys(m map[string]string) (names []string) {
+	for k := range m {
+		names = append(names, k)
+	}
+	sort.Strings(names)
+	return
+}
---
+++ billsreader/billsreader.go
@@ -0,0 +1,57 @@
+package billsreader
+
+import (
+	"bufio"
+	"fmt"
+	bp "github.com/luketower/roomies/billprocessor"
+	a "github.com/mgutz/ansi"
+	//"log"
+	"os"
+	//"os/exec"
+	"strings"
+)
+
+func errMsg(args []string) string {
+	return "\n" + a.Color(lineBreak("*", 50), "yellow") +
+		a.Color(lineBreak("*", 50), "yellow") + "\n" +
+		a.Color("There was an error processing your input:\n", "red") +
+		"  '" + strings.Join(args, " ") + "'\n\n" +
+		a.Color("  Ex. 'readbills <filename>'\n\n", "red") +
+		a.Color(lineBreak("*", 50), "yellow") +
+		a.Color(lineBreak("*", 50), "yellow")
+}
+
+func Run(args []string) {
+	if len(args) != 1 {
+		fmt.Printf("%s", errMsg(args))
+		os.Exit(1)
+	}
+	filename := args[0]
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Printf("OUCH! %s\n", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	scanner := bufio.NewScanner(reader)
+	readFile(scanner)
+}
+
+func lineBreak(char string, num int) string {
+	return strings.Repeat(char, num) + "\n"
+}
+
+func readFile(scanner *bufio.Scanner) {
+	for scanner.Scan() {
+		args := strings.Split(scanner.Text(), " ")
+		if isCommentOrBlankLine(args) {
+			continue
+		}
+		fmt.Println(bp.BillReport(args))
+	}
+}
+
+func isCommentOrBlankLine(args []string) bool {
+	return args[0] == "//" || args[0] == ""
+}

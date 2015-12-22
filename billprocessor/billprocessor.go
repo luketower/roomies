9b48e1f64cfadaecdc2eee623ab739a1be09e49d
Luke Tower <lukegtower@gmail.com>
2015-12-21 21:01:27 -0500

    Use time package instead of custom map

--- billprocessor/billprocessor.go
+++ billprocessor/billprocessor.go
@@ -8,30 +8,16 @@ import (
 	"sort"
 	"strconv"
 	"strings"
+	"time"
 )
 
-var (
-	DEFAULT_LINE_BREAK_LENGTH = 25
-	MAX_AMOUNT_LENGTH         = 15
-	MONTHS                    = map[string]string{
-		"01": "January",
-		"02": "February",
-		"03": "March",
-		"04": "April",
-		"05": "May",
-		"06": "June",
-		"07": "July",
-		"08": "August",
-		"09": "September",
-		"10": "October",
-		"11": "November",
-		"12": "December",
-	}
-	EXAMPLE_USAGE = "  'date 12/2015 gas 34.55 electric 45.99 rent 933 -- bob 45 susan 55'\n\n" +
-		"* You must include the date!\n" +
-		"* You must add '--' followed by name/percentage pairs.\n" +
-		"  Ex. '(args) -- bob 45 susan 55'\n\n"
-)
+var DEFAULT_LINE_BREAK_LENGTH = 25
+var MAX_AMOUNT_LENGTH = 15
+var EXAMPLE_USAGE = "  " +
+	"'date 12/2015 gas 34.55 electric 45.99 rent 933 -- bob 45 susan 55'\n\n" +
+	"* You must include the date!\n" +
+	"* You must add '--' followed by name/percentage pairs.\n" +
+	"  Ex. '(args) -- bob 45 susan 55'\n\n"
 
 func ErrorMsg(args []string) string {
 	yellowLine := line.Make("*", 70, "yellow")
@@ -116,7 +102,8 @@ func parse(args []string) (d argsData) {
 func makeHeader(word string, args []string, i int) (header string) {
 	if strings.Contains(word, "/") && isDate(args[i-1]) {
 		monthAndYear := strings.Split(word, "/")
-		header = MONTHS[monthAndYear[0]] + " " + monthAndYear[1]
+		monthInt := parseInt(monthAndYear[0])
+		header = time.Month(monthInt).String() + " " + monthAndYear[1]
 	} else {
 		header = strings.Title(strings.Replace(word, "-", " ", -1))
 	}
@@ -142,6 +129,14 @@ func calcShare(percent string, total float64) float64 {
 
 func parseFloat(num string) (f float64) {
 	f, err := strconv.ParseFloat(num, 64)
+	if err != nil {
+		log.Fatal("OUCH! ", err)
+	}
+	return
+}
+
+func parseInt(num string) (i int64) {
+	i, err := strconv.ParseInt(num, 10, 64)
 	if err != nil {
 		log.Fatal("OUCH! ", err)
 	}

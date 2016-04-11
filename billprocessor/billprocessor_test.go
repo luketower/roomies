478ccc2eb7494c89d464baee3f40b9bd7445c1ab
Luke Tower <lukegtower@gmail.com>
2016-04-10 23:57:03 -0400

    Clean up tests

--- billprocessor/billprocessor_test.go
+++ billprocessor/billprocessor_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-// Test-helpers
+// Test Helpers
 
 func compareStrings(result, expected, methodName string, t *testing.T) {
 	if result != expected {
@@ -19,24 +19,24 @@ func compareStrings(result, expected, methodName string, t *testing.T) {
 func TestHasValid(t *testing.T) {
 	validArgs := []string{"date", "03/2015", "gas", "34.56",
 		"--", "bob", "55", "susan", "45"}
+	wrongNumOfArgs := []string{"date", "03/2015", "gas", "34.56", "electric",
+		"--", "bob", "55", "susan", "45"}
+	noDateArgs := []string{"03/2015", "gas", "34.56",
+		"--", "bob", "55", "susan", "45"}
+	noBillsArgs := []string{"date", "03/2015", "--", "bob", "55", "susan", "45"}
 
 	if !HasValid(validArgs) {
 		t.Errorf("\nArgs are valid but code says NOT: %q", validArgs)
 	}
 
-	wrongNumOfArgs := []string{"date", "03/2015", "gas", "34.56", "electric",
-		"--", "bob", "55", "susan", "45"}
 	if HasValid(wrongNumOfArgs) {
 		t.Errorf("\nWrong number of args: %q", wrongNumOfArgs)
 	}
 
-	noDateArgs := []string{"03/2015", "gas", "34.56", "electric",
-		"--", "bob", "55", "susan", "45"}
 	if HasValid(noDateArgs) {
 		t.Errorf("\nImproper date format in args: %q", noDateArgs)
 	}
 
-	noBillsArgs := []string{"date", "03/2015", "--", "bob", "55", "susan", "45"}
 	if HasValid(noBillsArgs) {
 		t.Errorf("\nNo bills in args: %q", noBillsArgs)
 	}
@@ -49,21 +49,20 @@ func TestMakeHeader(t *testing.T) {
 		"--", "bob", "55", "susan", "45"}
 	customArgs := []string{"header", "03/2015", "gas", "34.56",
 		"--", "bob", "55", "susan", "45"}
-	dateHeader := makeHeader("03/2015", dateArgs, 1)
-	monthHeader := makeHeader("03/2015", monthArgs, 1)
-	customHeader := makeHeader("03/2015", customArgs, 1)
+
 	expectedDate := "March 2015"
 	expectedMonth := "March 2015"
 	expectedCustom := "03/2015"
-	if dateHeader != expectedDate {
+
+	if dateHeader := makeHeader("03/2015", dateArgs, 1); dateHeader != expectedDate {
 		t.Errorf("\nGot: %q\nExpected: %q\nargs: %q", dateHeader,
 			expectedDate, dateArgs)
 	}
-	if monthHeader != expectedMonth {
+	if monthHeader := makeHeader("03/2015", monthArgs, 1); monthHeader != expectedMonth {
 		t.Errorf("\nGot: %q\nExpected: %q\nargs: %q", monthHeader,
 			expectedMonth, monthArgs)
 	}
-	if customHeader != expectedCustom {
+	if customHeader := makeHeader("03/2015", customArgs, 1); customHeader != expectedCustom {
 		t.Errorf("\nGot: %q\nExpected: %q\nargs: %q", customHeader,
 			expectedCustom, customArgs)
 	}
